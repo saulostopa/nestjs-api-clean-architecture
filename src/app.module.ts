ae334b7afb9388d609ac4589a0ea702634c286a1
Saulo Stopa <saulostopa@gmail.com>
2025-12-16 20:50:57 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
-import { UsersHttpModule } from '@modules/users/http/users-http.module'
-import { DeliveriesHttpModule } from '@modules/deliveries/http/deliveries-http.module'
+import { UsersHttpModule } from './modules/users/http/users-http.module'
+import { DeliveriesHttpModule } from './modules/deliveries/http/deliveries-http.module'
 
 @Module({
   imports: [
